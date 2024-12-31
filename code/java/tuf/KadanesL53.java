605a0b6f05906595ed2b2bec8f9303a2a6fe006c
purus63 <2014dib021@gmail.com>
2024-12-31 20:21:59 +0530

    kadanes

---
+++ code/java/tuf/KadanesL53.java
@@ -0,0 +1,15 @@
+package tuf;
+
+public class KadanesL53 {
+        public int maxSubArray(int[] nums) {
+            int cursum=nums[0];
+            int maxsum=nums[0];
+
+            for(int i=1;i<nums.length;i++)
+            {
+                cursum =Math.max(cursum+nums[i],nums[i]);
+                maxsum=Math.max(maxsum,cursum);
+            }
+            return maxsum;
+        }
+}
